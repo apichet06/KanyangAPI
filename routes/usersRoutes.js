a39650c023fc11b19dc32774b97077b6bf379a41
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-04-25 16:08:34 +0700

    8rd commit

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -7,7 +7,7 @@ const routes = express.Router()
 
 routes.get('/', usersController.GetUsersAll)
 routes.get('/:u_number', auth.authenticateToken, usersController.GetUsersById)
-routes.post('/', auth.authenticateToken, usersController.CreateUsers)
+routes.post('/', usersController.CreateUsers)
 routes.put('/:u_number', auth.authenticateToken, usersController.UpdateUsers)
 routes.delete('/:u_number', auth.authenticateToken, usersController.DeleteUsers)
 routes.post('/login', usersController.Login);
