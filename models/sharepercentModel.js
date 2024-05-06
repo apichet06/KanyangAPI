0f0977310e6b65496e7c632bf581f0c09711341b
Apichet Singnakrong <50052243+apichet06@users.noreply.github.com>
2024-05-06 16:50:05 +0700

    8rd commit

--- models/sharepercentModel.js
+++ models/sharepercentModel.js
@@ -48,32 +48,36 @@ class sharepercentModel {
 
     static async getShareAll() {
         try {
+
+
+
             const [result] = await db.query(
-                `SELECT  year(b.w_datetime) as year_w_datetime,a.u_number,
-                    Max(a.u_title) as u_title,
-                    Max(a.u_firstname) as u_firstname,
-                    Max(a.u_lastname) as u_lastname,
-                    MAX(CONCAT(a.u_address ,' ต.',e.name_in_thai,' อ.',d.name_in_thai,' จ.',c.name_in_thai, ' ', e.zip_code)) AS u_address,
-                    Max(a.u_share) as u_share,
-                    Max(( SELECT s_percent FROM kanyangDB.share_percent WHERE s_year = 2024)) AS percent,
-                    ROUND(Max(((SELECT s_percent FROM kanyangDB.share_percent WHERE s_year = 2024) * a.u_share)/100),2) AS shareCount,
-                    COALESCE(SUM(b.w_weigth),0) AS weightSum,
-                    Max(( SELECT s_huatun FROM kanyangDB.share_percent WHERE s_year = 2024)) AS percent_yang,
-                    ROUND(COALESCE(SUM((SELECT SUM(s_huatun/1000) FROM kanyangDB.share_percent WHERE s_year = 2024) * b.w_weigth), 0),2) AS weightPriceSum,
-                    ROUND(COALESCE(Max(((SELECT s_percent FROM kanyangDB.share_percent WHERE s_year = 2024) * a.u_share)/100),0) +
-                    COALESCE(SUM((SELECT SUM(s_huatun/1000) FROM kanyangDB.share_percent WHERE s_year = 2024) * b.w_weigth), 0),2)
-                AS sumPrice
-                FROM kanyangDB.Users a 
-                LEFT JOIN kanyangDB.weight_price b
-                ON a.u_number = b.u_number
-                INNER JOIN kanyangDB.provinces c
-                ON c.id = a.provinces_id
-                INNER JOIN kanyangDB.districts d
-                ON d.id = a.districts_id
-                INNER JOIN kanyangDB.subdistricts e
-                ON e.id = a.subdistricts_id
-                GROUP BY a.u_number,year(b.w_datetime)
-                ORDER BY a.u_number,year(b.w_datetime) desc`
+                `SELECT 
+                YEAR(c.r_rubber_date) AS r_rubber_year,
+                a.u_number,
+                MAX(a.u_title) AS u_title,
+                MAX(a.u_firstname) AS u_firstname,
+                MAX(a.u_lastname) AS u_lastname,
+                MAX(a.u_share) AS u_share,
+                MAX(CONCAT(a.u_address ,' ต.',g.name_in_thai,' อ.',f.name_in_thai,' จ.',e.name_in_thai, ' ', g.zip_code)) AS u_address,
+                ROUND(COALESCE(Max(d.s_percent),0),2) AS percent, 
+                ROUND(COALESCE(Sum(a.u_share*d.s_percent/100),0),2) AS Sumpercentshare,
+                ROUND(COALESCE(Sum(b.w_weigth),0),2) AS Sumweight,
+                ROUND(COALESCE(Max(d.s_huatun),0),2) AS percent_yang,
+                ROUND(COALESCE(Sum((d.s_huatun/1000)*b.w_weigth),0),2) AS sumhuatun, 
+                ROUND(COALESCE(Sum((a.u_share*d.s_percent/100)+(d.s_huatun/1000)*b.w_weigth),0),2) AS sumPrice
+            FROM kanyangDB.Users a
+            LEFT JOIN kanyangDB.weight_price b  ON a.u_number = b.u_number
+            LEFT JOIN kanyangDB.rubber_price c  ON b.r_number = c.r_number
+            LEFT JOIN kanyangDB.share_percent d ON year(c.r_rubber_date) = d.s_year
+            INNER JOIN kanyangDB.provinces e 	ON e.id = a.provinces_id
+            INNER JOIN kanyangDB.districts f    ON f.id = a.districts_id
+            INNER JOIN kanyangDB.subdistricts g ON g.id = a.subdistricts_id
+            WHERE  YEAR(c.r_rubber_date) like '%%' and a.u_number like '%%'
+            GROUP BY 
+                YEAR(c.r_rubber_date), 
+                a.u_number
+            order by a.u_number asc`
             )
             if (result)
                 return result
